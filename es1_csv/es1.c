e42f1d8441ff24e4916565a12bcfc90230a7a0bb
Ludovico Leardi <ludovico.leardi@itiscuneo.eu>
2022-09-19 11:13:40 +0200

    Aggiunta lettura prime due colonne con fscanf.

--- es1_csv/es1.c
+++ es1_csv/es1.c
@@ -2,19 +2,17 @@
 #include <stdlib.h>
 
 #define SIZE 1024
-
-typedef struct
-{
-
-} Tabella;
+#define LENGHT 16600
 
 int main()
 {
-    char riga[SIZE];
+    int rank[LENGHT];
+    char name[SIZE][LENGHT];
+    int cont = 0;
 
     FILE *fp;
     fp = fopen("vgsales.csv", "r");
-
+    
     if(fp == NULL)
     {
         printf("Il file non esiste!\n");
@@ -25,7 +23,9 @@ int main()
 
     while(!feof(fp))
     {
-        fgets(riga, SIZE, fp);
+        fscanf(fp, "%d,%s", &rank[cont], name[cont]);
+        printf("%d,%s", rank[cont], name[cont]);
+        cont ++;
     }
 
     fclose(fp);
